4c151cb9e324ccc35eac11674d80232719472947
Awais Memon <awaismemon26@gmail.com>
2022-07-01 20:51:32 +0200

    add cloud storage stuff

--- app.py
+++ app.py
@@ -1,11 +1,66 @@
+from google.api_core.exceptions import NotFound
+from logging import Logger
 import os
 import redis
+from google.cloud import storage
 
 from flask import Flask
+import logging
+from logging.config import fileConfig
+
+fileConfig('logger.cfg')
+LOGGER = logging.getLogger()
+
+if 'LOG_LEVEL' in os.environ:
+    log_levels = {'NOTSET': 0, 'DEBUG': 10, 'INFO': 20,
+                  'WARN': 30, 'ERROR': 40, 'CRITICAL': 50}
+    if os.environ.get('LOG_LEVEL') in log_levels:
+        LOGGER.setLevel(log_levels[os.environ.get('LOG_LEVEL')])
+    else:
+        LOGGER.error(
+            f'LOG_LEVEL {os.environ.get("LOG_LEVEL")} is not a valid level. using {LOGGER.level}')
+else:
+    LOGGER.warning(f'LOG_LEVEL not set. current log level is {LOGGER.level}')
+
 
 app = Flask(__name__)
 
 r = None
+
+
+@app.route("/download/<value>")
+def download_file(value):
+    download_from_bucket("test-python-001", f"{value}.jpeg")
+    if os.path.isfile(f"{value}.jpeg"):
+        return f'''
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+        </head>
+
+        <body>
+            <h1>Downloaded image</h1>
+            <img src="{value}.jpeg" alt="{value}.jpeg" width="500" height="400">
+        </body>
+
+        </html>
+
+        ''', 200
+    return "NotFound", 404
+
+
+def download_from_bucket(bucket_name, file_name, folder_path=None):
+    try:
+        client = storage.Client()
+        bucket = client.get_bucket(bucket_name)
+        full_path = os.path.join(
+            folder_path, file_name) if folder_path else file_name
+        blob = bucket.blob(full_path)
+        blob.download_to_filename(file_name)
+    except NotFound as e:
+        Logger.error(f'file {full_path} not found in bucket {bucket_name}')
+        Logger.error(e)
 
 
 @app.route("/")
